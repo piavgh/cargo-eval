5f67b7e3392faad1d413e96f859986ce659e9763
ko1N <ko1N1337@gmail.com>
2019-09-13 14:32:58 +0200

    Fixed compilation on rust 1.35.0

--- src/main.rs
+++ src/main.rs
@@ -244,11 +244,11 @@ fn try_main() -> Result<i32> {
             Input::File(&script_name, &script_path, &content, mtime)
         },
         (Some(expr), true, false) => {
-            content = expr.into();
+            content = expr.clone().into();
             Input::Expr(&content, args.template.as_ref().map(|s| &**s))
         },
         (Some(loop_), false, true) => {
-            content = loop_.into();
+            content = loop_.clone().into();
             Input::Loop(&content, args.count)
         },
         _ => unreachable!(),
